819eb87855bdacb14110bc4d42a10924f3016ed2
Amer-bit <https://github.com/Amer-bit>
2020-02-18 08:07:11 +0200

    aboutme

---
+++ js/app.js
@@ -0,0 +1,35 @@
+'use strict'
+
+
+var clientName = prompt('hey, I would like to know your name');
+console.log(clientName);
+
+alert('Welcome ' + clientName + ' in this page which made for adventurous');
+/*do you like traveling to space
+  do you like climbing mountains
+  do you like skydiving
+  do you like diving deep in the sea */
+var spaceTravelingQuestion;
+spaceTravelingQuestion = prompt('Do you like traveling to space');
+console.log(spaceTravelingQuestion);
+
+var climbingMountaingQuestion = prompt('Do like climbing mountains');
+console.log(climbingMountaingQuestion);
+
+var skydivingQuestion = prompt('do you like skydiving');
+console.log(skydivingQuestion);
+
+var seaDiving = prompt('do you like diving deep in the sea');
+console.log(seaDiving);
+
+// while (spaceTravelingQuestion !== 'yes' || spaceTravelingQuestion !== 'y' || spaceTravelingQuestion !== 'no' || spaceTravelingQuestion !== 'n')
+//     alert('Please type yes/no or y/n');
+
+
+// while ((spaceTravelingQuestion = 'yes' || spaceTravelingQuestion == 'y') && (spaceTravelingQuestion != 'no' || spaceTravelingQuestion != 'n'))
+// {alert('Please type yes/no or y/n');
+// spaceTravelingQuestion = prompt('Do you like traveling to space');}
+//while ((spaceTravelingQuestion !== 'y' || spaceTravelingQuestion !== 'n') || (spaceTravelingQuestion !== 'yes' || spaceTravelingQuestion !== 'no')) {
+  // alert("please enter yes/no or y/n");
+  // spaceTravelingQuestion = prompt('dp');
+  // }
