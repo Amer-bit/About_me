980a13112ffe93f59722a0039b37f6c38521b299
Amer-bit <https://github.com/Amer-bit>
2020-02-18 11:55:02 +0200

    Finally working

--- js/app.js
+++ js/app.js
@@ -12,53 +12,66 @@ alert('Welcome ' + clientName + ' in this page which made for adventurous');
 var spaceTravelingQuestion;
 spaceTravelingQuestion = prompt('Am I a space man').toUpperCase();
 console.log(spaceTravelingQuestion);
-if (spaceTravelingQuestion === ('YES' || 'Y')) {
+if (spaceTravelingQuestion === 'YES' || spaceTravelingQuestion === 'Y') {
   alert('No but i would like to be one');
 }
-else if (spaceTravelingQuestion === ('NO' || 'N')) {
+else if (spaceTravelingQuestion == 'NO' || spaceTravelingQuestion === 'N') {
   alert('That\'s correct this is sad i wanted to try the 0 GForce feeling');
 }
 else {
   alert('Please submit one of the following: y/n or yes/no');
 }
 
-var climbingMountaingQuestion = prompt('Am i mountain climber').toUpperCase();
+var climbingMountaingQuestion = prompt('Do i love Mountain Climbing').toUpperCase();
 console.log(climbingMountaingQuestion);
-if (climbingMountaingQuestion === ('YES' || 'Y')) {
-  alert('No this is too scary');
+if (climbingMountaingQuestion === 'YES' || climbingMountaingQuestion === 'Y') {
+  alert('Hell Yeah');
 }
-else if (climbingMountaingQuestion === ('NO' || 'N')) {
-  alert('You are right');
+else if (climbingMountaingQuestion === 'NO' || climbingMountaingQuestion === 'N') {
+  alert('Im not a puss dude');
 }
 else {
   alert('Please submit one of the following: y/n or yes/no');
 }
 
-var skydivingQuestion = prompt('Am i a skydiver').toUpperCase();
+var skydivingQuestion = prompt('Am i a Skydiver').toUpperCase();
 console.log(skydivingQuestion);
-if (skydivingQuestion === ('YES' || 'Y')) {
-  alert('Yes i dreamed about skydiving and this count');
-} else if (skydivingQuestion === ('NO' || 'N')) {
+if (skydivingQuestion === 'YES' || skydivingQuestion === 'Y') {
+  alert('Yes i dreamed about Skydiving and this count');
+}
+else if (skydivingQuestion === 'NO' || skydivingQuestion === 'N') {
   alert('You are wrong OBVIOUSLY');
 }
 else {
   alert('Please submit one of the following: y/n or yes/no');
 }
 
-var seaDiving = prompt('Do i like sea diving').toUpperCase();
+var mountainBiking = prompt('Is Mountain Biking my favourrite sport').toUpperCase();
+console.log(mountainClimbing);
+if(mountainBiking ==='Y' || mountainBiking === 'YES'){
+prompt('Hell Yeah');
+}
+else if(mountainBiking ==='NO' || mountainBiking ==='N')
+{
+  prompt('You are wrong Bruh');
+}
+else{
+  alert('Please submit one of the following: y/n or yes/no');
+}
+
+
+var seaDiving = prompt('Do i like Seadiving').toUpperCase();
 console.log(seaDiving);
 console.log(typeof seaDiving);
-if (seaDiving === ('YES' || 'Y')) {
+if (seaDiving === 'YES' || seaDiving === 'Y') {
   alert('You are correct Damn do you know me Bruh');
-} else if (seaDiving === ('NO' || 'N')) {
+}
+else if (seaDiving === 'NO' || seaDiving === 'N') {
   alert('No thats wrong');
 }
 else {
   alert('Please submit one of the following: y/n or yes/no');
 }
-
-
-
 
 
 
